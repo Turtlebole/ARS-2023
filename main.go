3216acfce442e21dfe342a1d110e1376926ab3cc
Turtlebole <babic.bojan62@gmail.com>
2023-04-19 20:10:02 +0200

    Added Config and Services

--- main.go
+++ main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+
+	routerChan := mux.NewRouter()
+	routerChan.StrictSlash(true)
+
+	server := configServer{
+		data: map[string]*Config{},
+	}
+	routerChan.HandleFunc("/post/", server.createPostHandler).Methods("POST")
+	routerChan.HandleFunc("/posts/", server.getAllHandler).Methods("GET")
+	routerChan.HandleFunc("/post/{id}/", server.getPostHandler).Methods("GET")
+	routerChan.HandleFunc("/post/{id}/", server.delPostHandler).Methods("DELETE")
+
+	// start server
+	srv := &http.Server{Addr: "0.0.0.0:8000", Handler: routerChan}
+	go func() {
+		log.Println("server starting")
+		if err := srv.ListenAndServe(); err != nil {
+			if err != http.ErrServerClosed {
+				log.Fatal(err)
+			}
+		}
+	}()
+
+	<-quit
+
+	log.Println("service shutting down ...")
+
+	// gracefully stop server
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("server stopped")
 }
