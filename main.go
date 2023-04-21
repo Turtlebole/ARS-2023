bc31a7455d1d9fa3ae5388194d646949db23b291
Turtlebole <babic.bojan62@gmail.com>
2023-04-21 10:41:38 +0200

    main.go updated

--- main.go
+++ main.go
@@ -22,10 +22,10 @@ func main() {
 	server := configServer{
 		data: map[string]*Config{},
 	}
-	routerChan.HandleFunc("/post/", server.createPostHandler).Methods("POST")
-	routerChan.HandleFunc("/posts/", server.getAllHandler).Methods("GET")
-	routerChan.HandleFunc("/post/{id}/", server.getPostHandler).Methods("GET")
-	routerChan.HandleFunc("/post/{id}/", server.delPostHandler).Methods("DELETE")
+	routerChan.HandleFunc("/config/", server.createPostHandler).Methods("POST")
+	routerChan.HandleFunc("/configs/", server.getAllHandler).Methods("GET")
+	routerChan.HandleFunc("/config/{id}/", server.getPostHandler).Methods("GET")
+	routerChan.HandleFunc("/config/{id}/", server.delPostHandler).Methods("DELETE")
 
 	// start server
 	srv := &http.Server{Addr: "0.0.0.0:8000", Handler: routerChan}
