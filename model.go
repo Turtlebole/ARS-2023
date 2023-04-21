e25c48d64475007c8e217269c3e2da1b3b10af71
Turtlebole <babic.bojan62@gmail.com>
2023-04-21 10:39:57 +0200

    Delete config service added

--- model.go
+++ model.go
@@ -2,12 +2,9 @@ package main
 
 type Config struct {
 	Id      string            `json:"id"`
-	Title   string            `json:"title"`
-	Text    string            `json:"text"`
-	Tags    []string          `json:"tags"`
 	Entries map[string]string `json:"entries"`
 }
 
 type Service struct {
-	Data map[string]*Config `json:"data"`
+	Data map[string]*Config
 }
